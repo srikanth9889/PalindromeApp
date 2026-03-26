e8a03049fc182daa21645a41c258d8fac7fa02c2
srikanth9889 <sripandi040@gmail.com>
2026-03-26 10:20:20 +0530

    UC2: Hardcoded palindrome

--- src/UseCase2PalindromeCheckerApp.java
+++ src/UseCase2PalindromeCheckerApp.java
@@ -1,2 +1,18 @@
 public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String str = "madam";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if (str.equals(rev)) {
+            System.out.println("The given string is Palindrome");
+        } else {
+            System.out.println("The given string is Not Palindrome");
+        }
+    }
 }
